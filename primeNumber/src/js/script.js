6dd5b3cba210c6e81fe4f4668a079c655669e8f4
rasulovagozel <rasulovagozl411@gmail.com>
2023-07-13 15:23:43 +0400

    add js file

---
+++ primeNumber/src/js/script.js
@@ -0,0 +1,17 @@
+let a = +prompt("Enter the numberI");
+
+if(a>0){
+for(let i=2;i<a;i++){
+    if(a%i==0){
+        console.log(`${a} is not a prime number.`);
+        break
+    }
+    else{
+        console.log(`${a} is a prime number.`);
+        break;
+    }
+}
+}
+else{
+    console.log(`${a} is neither a prime nor a complex number.`);
+}
